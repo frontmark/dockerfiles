5d783ce604fdabb8c723373559e3390680129ec0
favmd <m.dietl@frontmark.de>
2022-12-07 17:06:47 +0100

    Update views.py

--- python-gunicorn-flask-app/fAPP/views.py
+++ python-gunicorn-flask-app/fAPP/views.py
@@ -4,30 +4,33 @@ from flask import (
     Flask,
     make_response,
     render_template,
-    #send_from_directory,
+    # send_from_directory,
 )
 from flask_compress import Compress
 
 app = Flask(__name__, static_url_path="/appname/static")
 Compress(app)
 
+
 @app.context_processor
 def inject_now():
     return dict(now=datetime.now())
 
-#@app.route("/favicon.ico")
-#def favicon_ico():
+
+# @app.route("/favicon.ico")
+# def favicon_ico():
 #    return send_from_directory("public", "favicon.ico")
 
-#@app.route("/webclip.png")
-#def webclip_png():
+# @app.route("/webclip.png")
+# def webclip_png():
 #    return send_from_directory("public", "webclip.png")
+
 
 @app.route("/appname")
 def index():
     resp = make_response(
         render_template(
             "index.html",
-	)
+        )
     )
     return resp
